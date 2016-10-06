b2e5ba325a9ed59c13ae4da815505102a8a8d4a0
Zachary Robbins <xaqiri@outlook.com>
2016-10-06 14:05:09 -0500

    Initial commit
    Feature-complete line-of-sight test

---
+++ src/brick.py
@@ -0,0 +1,16 @@
+import pygame as p 
+
+class Brick(): 
+    def __init__(self, pos, size, color, font): 
+        self.pos = pos 
+        self.size = size 
+        self.color = color 
+        self.icon = '#' if self.color == (100, 100, 100) else '.' 
+        self.font = font 
+        self.id = 'wall' if self.icon == '#' else 'floor' 
+
+    def render(self, SCREEN, render_mode): 
+        if render_mode: 
+            p.draw.rect(SCREEN, self.color, (self.pos, self.size)) 
+        else: 
+            SCREEN.blit(self.font.render(self.icon, 1, self.color), (self.pos, self.size)) 
